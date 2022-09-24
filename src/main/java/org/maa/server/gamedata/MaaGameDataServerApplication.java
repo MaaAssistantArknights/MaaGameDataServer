aa6a19da84f3a54d5042d9adaf6d9f13c3d7af68
guigui <atmzxl@163.com>
2022-09-24 23:52:59 +0800

    移除fsjson，移除quartz，修改JDK版本，增加了一个schedule示例

--- src/main/java/org/maa/server/gamedata/MaaGameDataServerApplication.java
+++ src/main/java/org/maa/server/gamedata/MaaGameDataServerApplication.java
@@ -3,8 +3,11 @@ package org.maa.server.gamedata;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@MapperScan("org.maa.server.gamedata.mapper")
+
+@EnableScheduling
 @SpringBootApplication
 public class MaaGameDataServerApplication {
 

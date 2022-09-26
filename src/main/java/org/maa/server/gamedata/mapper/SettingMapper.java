3a379ed6220319916d03d0621e1e8b37a3bc8b6e
guigui <atmzxl@163.com>
2022-09-26 20:58:19 +0800

    feat: Mybatis增强Mapper和Query

--- src/main/java/org/maa/server/gamedata/mapper/SettingMapper.java
+++ src/main/java/org/maa/server/gamedata/mapper/SettingMapper.java
@@ -1,9 +1,16 @@
 package org.maa.server.gamedata.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.maa.server.gamedata.model.Setting;
+import org.maa.server.gamedata.query.QueryWrapperX;
+
+import java.util.List;
 
 @Mapper
-public interface SettingMapper extends BaseMapper<Setting> {
+public interface SettingMapper extends BaseMapperX<Setting> {
+
+    default List<Setting> selectByKey(String key) {
+        return selectList(new QueryWrapperX<Setting>().eqIfPresent("key", key));
+    }
+
 }
